c23d1c12667ae16a8772b04b505c66800d47a761
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2020-12-07 18:06:40 -0800

    use JI_NOTATION_COMMA_CLASS_IDS

--- src/notatingSymbolClasses/notatingSymbolClassIds.ts
+++ src/notatingSymbolClasses/notatingSymbolClassIds.ts
@@ -6,17 +6,21 @@ import {
     Scamon,
     TWO_3_FREE,
 } from "@sagittal/general"
-import {CommaClassId, getCommaClass, JI_NOTATION, SymbolClass, SymbolClassId, SYMBOL_CLASSES} from "@sagittal/system"
+import {
+    CommaClassId,
+    getCommaClass,
+    JI_NOTATION_COMMA_CLASS_IDS,
+    SymbolClass,
+    SymbolClassId,
+    SYMBOL_CLASSES,
+} from "@sagittal/system"
 
 // Different than findNotatingCommas because this one only concerns itself with commas that are in Sagittal
 const computeNotatingSymbolClassIds = ({monzo}: Scamon<{rational: true}>): SymbolClassId[] => {
     const notatingSymbolClassIds: SymbolClassId[] = []
     const two3FreeRationalMonzo = computeRoughRationalMonzo(monzo, TWO_3_FREE)
 
-    JI_NOTATION.forEach((commaClassId: CommaClassId): void => {
-        // TODO: POST-NOTATION-GENERATION: JI NOTATION CLEAN-UP
-        //  What this should really do is just scan over the JI Notation's symbol class IDs
-        //  At which time you can hide the SYMBOL_CLASSES from the outside world again
+    JI_NOTATION_COMMA_CLASS_IDS.forEach((commaClassId: CommaClassId): void => {
         const commaClass = getCommaClass(commaClassId)
         const commaClassTwo3FreeMonzo = computeRoughRationalMonzo(commaClass.pitch.monzo, TWO_3_FREE)
 
