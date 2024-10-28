ce5e0029c664f7a475ff69560a4e7ffb2b7c5362
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2024-10-27 23:26:59 -0700

    index of

--- src/notatingSymbolClasses/properties.ts
+++ src/notatingSymbolClasses/properties.ts
@@ -1,24 +1,24 @@
-import {Index, Two3FreeClass} from "@sagittal/general"
+import { Index, Two3FreeClass, indexOf } from "@sagittal/general"
 import {
     getSmallestSymbolSubsetId,
     SIZE_SORTED_SYMBOL_SUBSET_IDS,
     SymbolClassId,
     SymbolSubsetId,
 } from "@sagittal/system"
-import {computeNotatingSymbolClassIds} from "./notatingSymbolClassIds"
-import {NotatingSymbolClassesProperties} from "./types"
+import { computeNotatingSymbolClassIds } from "./notatingSymbolClassIds"
+import { NotatingSymbolClassesProperties } from "./types"
 
 const computeNotatingSymbolClassesProperties = (
     two3FreeClass: Two3FreeClass,
 ): NotatingSymbolClassesProperties => {
     const notatingSymbolClassIds = computeNotatingSymbolClassIds(two3FreeClass)
 
-    const smallestSymbolSubsetIndices = notatingSymbolClassIds
-        .map((symbolClassId: SymbolClassId): Index<SymbolSubsetId> => {
+    const smallestSymbolSubsetIndices = notatingSymbolClassIds.map(
+        (symbolClassId: SymbolClassId): Index<SymbolSubsetId> => {
             // This used to not include Trojan, and the tables that have been shared on the forum reflect that.
-            return SIZE_SORTED_SYMBOL_SUBSET_IDS
-                .indexOf(getSmallestSymbolSubsetId(symbolClassId)) as Index<SymbolSubsetId>
-        })
+            return indexOf(SIZE_SORTED_SYMBOL_SUBSET_IDS, getSmallestSymbolSubsetId(symbolClassId))
+        },
+    )
 
     return {
         notatingSymbolClassIds,
@@ -26,6 +26,4 @@ const computeNotatingSymbolClassesProperties = (
     }
 }
 
-export {
-    computeNotatingSymbolClassesProperties,
-}
+export { computeNotatingSymbolClassesProperties }
